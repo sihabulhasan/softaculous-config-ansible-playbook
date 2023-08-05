f137de8b4e14e259cb29dfeb30005f4d7610db1a
Sihabul <sihabulhasan27@gmail.com>
2023-08-05 14:43:12 +0600

    update

---
+++ roles/softaculous-config/files/universal.custom.php
@@ -0,0 +1,11 @@
+<?php
+#rebranding
+$globals['sn'] = 'Company Name';
+$globals['wordpress_manager_title'] = 'WP Manager by Company Name';
+$globals['logo_url'] = 'Logo URL';
+$globals['favicon_logo'] = 'Favicon URL';
+$globals['soft_email'] = 'Admin Mail';
+$globals['from_email'] = 'Admin Mail';
+#disable end user backup option
+$globals['disable_backup_restore'] = 1;
+$globals['disable_auto_backup'] = 1;
